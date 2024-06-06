9df770699eb8e1f2e5d04a9f6c956f6f5c19ef19
Cedric Cannard <ccannard@protonmail.com>
2024-06-06 15:21:04 -0700

    plotDiff

--- plotDiff.m
+++ plotDiff.m
@@ -24,10 +24,11 @@ end
 
 color1 = [0, 0.4470, 0.7410];
 color2 = [0.8500, 0.3250, 0.0980];
+color3 = [0.4660, 0.6740, 0.1880];      % green
 
-% Variable 2
+% Variable 1
 n = size(data1,2);
-if strcmpi(method2, 'mean')
+if strcmpi(method1, 'mean')
     data1_mean = mean(data1,2,'omitnan');
 else
     data1_mean = trimmean(data1,20,2);
@@ -48,7 +49,7 @@ end
 
 % Variable 2
 n = size(data2,2);
-if strcmpi(method2, 'mean')
+if strcmpi(method1, 'mean')
     data2_mean = mean(data2,2,'omitnan');
 else
     data2_mean = trimmean(data2,20,2);
@@ -67,29 +68,66 @@ elseif strcmpi(method2,'CI')
     data2_CI = data2_mean' + (-tscore.*SE)'; % 95% confidence interval
 end
 
-% figure; set(gcf,'Color','w');
+% Difference
+if strcmpi(method1, 'mean')
+    data3_mean = mean(data1-data2,2,'omitnan');
+else
+    data3_mean = trimmean(data1-data2,20,2);
+end
+if strcmpi(method2,'SD')
+    SD = std(data1-data2,[],2,'omitnan');  % standard deviation
+    data3_CI(1,:) = data3_mean + SD;
+    data3_CI(2,:) = data3_mean - SD;
+elseif strcmpi(method2,'SE')
+    SE = std(data1-data2,[],2,'omitnan') ./ sqrt(n)';  % standard error
+    data3_CI(1,:) = data3_mean + SE;
+    data3_CI(2,:) = data3_mean - SE;
+elseif strcmpi(method2,'CI')
+    SE = std(data1-data2,[],2,'omitnan') ./ sqrt(n)';  % standard error
+    tscore = tinv([.025 .975],n-1);  % t-score
+    data3_CI = data3_mean' + (-tscore.*SE)'; % 95% confidence interval
+end
+
+
+figure; set(gcf,'Color','w');
+
+subplot(2,1,1); hold on;
 
 % Plot variable 1 (mean + CI)
 p1 = plot(xAxis,data1_mean,'LineWidth',2,'Color', color1);
 patch([xAxis fliplr(xAxis)], [data1_CI(1,:) fliplr(data1_CI(2,:))], ...
-    color1,'FaceAlpha',.4,'EdgeColor',color1,'EdgeAlpha',.7);
-set(gca,'FontSize',12,'layer','top'); 
-hold on;
+    color1,'FaceAlpha',.3,'EdgeColor',color1,'EdgeAlpha',.9);
+set(gca,'FontSize',11,'layer','top'); 
 
-% Data2 (mean + CI)
+
+% Plot variable 2 (mean + CI)
 p2 = plot(xAxis,data2_mean,'LineWidth',2,'Color', color2);
 patch([xAxis fliplr(xAxis)], [data2_CI(1,:) fliplr(data2_CI(2,:))], ...
-    color2,'FaceAlpha',.4,'EdgeColor',color2,'EdgeAlpha',.7);
+    color2,'FaceAlpha',.3,'EdgeColor',color2,'EdgeAlpha',.9);
 set(gca,'FontSize',12,'layer','top'); 
-% hold off;
+grid off; axis tight; hold on; box on
+ylabel('Power (db)')
+title(sprintf('%s + 95%% %s',method1,method2)); 
+
+% Plot difference (mean + CI)
+subplot(2,1,2)
+plot(xAxis, data3_mean,'LineWidth',2,'Color', color3);
+patch([xAxis fliplr(xAxis)], [data3_CI(1,:) fliplr(data3_CI(2,:))], ...
+    color3,'FaceAlpha',.6,'EdgeColor',color3,'EdgeAlpha',0.9);
+grid off; axis tight; box on
+ylabel('Difference (uV)')
+
+% Add dash line to mark the null hypothesis
+hold on; plot([xAxis(1) xAxis(end)], [0 0],'k--','LineWidth',1) % thick dash line highlighting H0
+ylabel('Difference','FontSize',11,'FontWeight','bold')
+xlabel("Frequency (Hz)",'FontSize',11,'FontWeight','bold')
 
 % Plot significance bar at the bottom
 if sigBars
     plotSigBar(h, xAxis);
 end
 
-legend([p1, p2], {data1Name,data2Name}, 'Orientation','vertical'); 
+legend([p1, p2], {data1Name,data2Name}, 'Orientation','vertical','Location','SouthWest'); 
 
 % grid on; 
-axis tight;  
-box on
+set(findall(gcf,'type','axes'),'fontSize',11,'fontweight','bold');
